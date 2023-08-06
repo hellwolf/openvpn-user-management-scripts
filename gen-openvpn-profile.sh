cb0db6bbfbce0dc05fbdf9f3bb7a7a356685980c
Miao Zhicheng <miao@decentral.ee>
2023-08-06 19:41:55 +0300

    do more sanity check

--- gen-openvpn-profile.sh
+++ gen-openvpn-profile.sh
@@ -1,31 +1,42 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-[ -z "$1" ] && exit 1
+oops () {
+    echo "$@" >&2
+    exit 1
+}
+
+[ -z "$1" ] && oops "user name not provided"
 cd $(dirname "$0")/../easy-rsa
 
 PORT=$(grep '^port' ../server.conf  | awk '{print $2}')
 PROTO=$(grep '^proto' ../server.conf  | awk '{print $2}')
+SERVER_CA_CERT_FILE=pki/ca.crt
+CLIENT_CERT_FILE=pki/issued/client-$1.crt
+CLIENT_KEY_FILE=pki/private/client-$1.key
+
+[ -f "$CLIENT_CERT_FILE" ] || oops client cert file not found: "$CLIENT_CERT_FILE"
+[ -f "${CLIENT_CERT_FILE}" ] || oops client key file not found: "$CLIENT_KEY_FILE"
 
 cat <<EOF
 remote `hostname` $PORT $PROTO
 nobind
 dev tun
-persist-tun 
-persist-key 
-compress 
-pull 
-tls-client 
+persist-tun
+persist-key
+compress
+pull
+tls-client
 
-<ca> 
-`cat pki/ca.crt`
+<ca>
+$(cat "${SERVER_CA_CERT_FILE}")
 </ca>
 
-<cert> 
-`cat pki/issued/client-$1.crt`
+<cert>
+$(cat "${CLIENT_CERT_FILE}")
 </cert>
 
 <key>
-`cat pki/private/client-$1.key`
+$(cat "${CLIENT_KEY_FILE}")
 </key>
 
 <tls-auth>
